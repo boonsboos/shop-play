80f54d09c07c74ce314333b3297e59e9ce9d5c7e
boonsboos <boons@boonsboos.nl>
2025-10-06 20:17:05 +0200

    add jdbc driver to query our db

---
+++ src/main/kotlin/nl/connectplay/scoreplay/data/Database.kt
@@ -0,0 +1,12 @@
+package nl.connectplay.scoreplay.data
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class Database {
+
+    // local DB only
+    public val connection: Connection? = DriverManager.getConnection(
+        "jdbc:mariadb://localhost:3306/score_play?user=root&password="
+    )
+}
--- src/main/kotlin/nl/connectplay/scoreplay/routes/examples/Get.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/routes/examples/Get.kt
@@ -3,12 +3,20 @@ package nl.connectplay.scoreplay.routes.examples
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
-import io.ktor.server.routing.route
+import nl.connectplay.scoreplay.data.Database
 import nl.connectplay.scoreplay.routes.ApiRoute
 
 @ApiRoute
 fun Route.exampleRoute() {
     get("/example") {
-        call.respond(mapOf("hello" to "world"))
+        Database().connection?.use { connection ->
+
+            val statement = connection.prepareStatement("SELECT user_id FROM users")
+            val result = statement.executeQuery()
+
+            call.respond(result?.last() ?: false)
+
+            connection.close()
+        }
     }
 }
