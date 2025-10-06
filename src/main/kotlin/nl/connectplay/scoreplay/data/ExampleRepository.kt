9f7fdf961d203f1b098dd771611458bcdf7a26b5
boonsboos <boons@boonsboos.nl>
2025-10-06 20:54:31 +0200

    add more comments

--- src/main/kotlin/nl/connectplay/scoreplay/data/ExampleRepository.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/data/ExampleRepository.kt
@@ -14,6 +14,8 @@ class ExampleRepository {
         // if you want to get a result back, you need to start a new coroutine
         return coroutineScope {
             // run your query asynchronously (without blocking the current thread)
+            // this means that while we are communicating with the database,
+            // Ktor can handle other requests
             val databaseResult = async {
                 val statement = connection?.prepareStatement("SELECT user_id FROM users")
                 val resultSet = statement?.executeQuery()
