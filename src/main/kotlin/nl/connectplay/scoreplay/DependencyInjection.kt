ba6f2076a9f0f6ecd0e2a8f2b226eede26ecd0ac
boons <57451966+boonsboos@users.noreply.github.com>
2025-10-16 18:13:29 +0200

    [FSA-79] fix DI for events (#26)

--- src/main/kotlin/nl/connectplay/scoreplay/DependencyInjection.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/DependencyInjection.kt
@@ -4,8 +4,10 @@ import io.ktor.server.config.ApplicationConfig
 import nl.connectplay.scoreplay.abstraction.data.ExampleRepository
 import nl.connectplay.scoreplay.abstraction.data.FriendRepository
 import nl.connectplay.scoreplay.abstraction.data.GameRepository
+import nl.connectplay.scoreplay.abstraction.data.NotificationRepository
 import nl.connectplay.scoreplay.abstraction.data.SessionRepository
 import nl.connectplay.scoreplay.abstraction.data.UserRepository
+import nl.connectplay.scoreplay.abstraction.services.EventQueueManagerService
 import nl.connectplay.scoreplay.abstraction.services.FriendService
 import nl.connectplay.scoreplay.abstraction.services.UserAccountService
 import nl.connectplay.scoreplay.controllers.ExampleController
@@ -19,6 +21,9 @@ import nl.connectplay.scoreplay.data.DatabaseSessionRepository
 import nl.connectplay.scoreplay.data.DatabaseUserRepository
 import nl.connectplay.scoreplay.options.JWTOptions
 import nl.connectplay.scoreplay.data.DatabaseGameRepository
+import nl.connectplay.scoreplay.data.DatabaseNotificationRepository
+import nl.connectplay.scoreplay.events.EventQueueManagerServiceImpl
+import nl.connectplay.scoreplay.events.EventRouter
 import nl.connectplay.scoreplay.services.FriendServiceImpl
 import nl.connectplay.scoreplay.services.UserAccountServiceImpl
 import org.koin.core.module.dsl.bind
@@ -36,6 +41,7 @@ fun repositories() = module {
     singleOf(::DatabaseUserRepository) { bind<UserRepository>() }
     singleOf(::DatabaseGameRepository) { bind<GameRepository>() }
     singleOf(::DatabaseFriendRepository) { bind<FriendRepository>() }
+    singleOf(::DatabaseNotificationRepository) { bind<NotificationRepository>() }
 }
 
 /**
@@ -54,6 +60,10 @@ fun controllers() = module {
 fun services() = module {
     singleOf(::FriendServiceImpl) { bind<FriendService>() }
     singleOf(::UserAccountServiceImpl) { bind<UserAccountService>() }
+
+    // events
+    singleOf(::EventRouter) { bind<EventRouter>() }
+    singleOf(::EventQueueManagerServiceImpl) { bind<EventQueueManagerService>() }
 }
 
 /**
