0633f767f534729f4fefd217673c89a33d23fe7a
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2025-10-22 23:56:21 +0200

    changed to look similair to example SessionVisibility

--- src/main/kotlin/nl/connectplay/scoreplay/models/ScoringMethod.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/models/ScoringMethod.kt
@@ -1,35 +1,34 @@
 package nl.connectplay.scoreplay.models
 
 /**
- * This enum class that implements [EnumAction].
- *
- * Each value defines its own implementation of [handle],
- * allowing different scoring strategies to be executed
- * depending on the selected enum value.
- *
  * Enum of all scoring methods a game can use.
  *
- * Use [id] for persistence and [name] for UI.
- * Look up by id with [fromId].
  */
-enum class ScoringMethod(
-    val id: Int,
-    val name: String
-) : EnumAction {
+enum class ScoringMethod {
+    HIGHEST_SCORE_WINS,
+    LOWEST_SCORE_WINS,
+    FIRST_TO_X_SCORE_WINS,
+    LAST_TO_X_SCORE_WINS,
+    FINISHING_ON_POSITION_Y_WINS,
+    LAST_MAN_STANDING_WINS;
 
-    HIGHEST_SCORE_WINS(1, "Highest score wins") {
-        override fun handle() = println(name)
-    },
-    LOWEST_SCORE_WINS(2, "Lowest score wins") {
-        override fun handle() = println(name)
-    },
-    FIRST_TO_X_SCORE_WINS(3, "First to X score wins") {
-        override fun handle() = println(name)
-    },
-    FINISHING_ON_POSITION_Y_WINS(4, "Finishing on position Y wins") {
-        override fun handle() = println(name)
-    },
-    LAST_MAN_STANDING_WINS(5, "Last man standing wins") {
-        override fun handle() = println(name)
-    };
+    fun toInt() = when (this) {
+        HIGHEST_SCORE_WINS -> 0
+        LOWEST_SCORE_WINS -> 1
+        FIRST_TO_X_SCORE_WINS -> 2
+        LAST_TO_X_SCORE_WINS -> 3
+        FINISHING_ON_POSITION_Y_WINS -> 4
+        LAST_MAN_STANDING_WINS -> 5
+    }
+
+    companion object {
+        fun fromInt(score: Int) = when (score) {
+            0 -> HIGHEST_SCORE_WINS
+            1 -> LOWEST_SCORE_WINS
+            2 -> FIRST_TO_X_SCORE_WINS
+            3 -> LAST_TO_X_SCORE_WINS
+            4 -> FINISHING_ON_POSITION_Y_WINS
+            5 -> LAST_MAN_STANDING_WINS
+        }
+    }
 }
