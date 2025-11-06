a1172906e3abf7b6d1922bbf16d6e367f0b71c0b
boonsboos <boons@boonsboos.nl>
2025-11-06 17:01:51 +0100

    fix

--- src/main/kotlin/nl/connectplay/scoreplay/data/Database.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/data/Database.kt
@@ -1,15 +1,18 @@
 package nl.connectplay.scoreplay.data
 
+import nl.connectplay.scoreplay.options.DBOptions
 import org.mariadb.jdbc.MariaDbPoolDataSource
 import java.sql.Connection
 
-class Database {
+class Database(private val dbOptions: DBOptions) {
 
     // local DB only
     // pool the database connections to reuse connections
-    private var _pool: MariaDbPoolDataSource = MariaDbPoolDataSource(
-        "jdbc:mariadb://db.connectplay.local/score_play?user=root&password="
-    )
+    private var _pool: MariaDbPoolDataSource
+
+    init {
+        _pool = MariaDbPoolDataSource(dbOptions.connString)
+    }
 
     val connection: Connection? get() = _pool.connection
 
