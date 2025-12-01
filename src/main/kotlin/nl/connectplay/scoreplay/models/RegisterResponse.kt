8afe5d1e9836ea2c297107da016dc0e2ed46d094
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2025-12-01 20:50:46 +0100

    fix import of RegisterResponse.kt

--- src/main/kotlin/nl/connectplay/scoreplay/models/RegisterResponse.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/models/RegisterResponse.kt
@@ -1,8 +1,10 @@
+package nl.connectplay.scoreplay.models
+
 import kotlinx.serialization.Serializable
 import nl.connectplay.scoreplay.models.dto.user.UserDto
 
 @Serializable
-data class RegisterResponse(
+open class RegisterResponse(
     val data: UserDto? = null,
     val message: String? = null
 )
