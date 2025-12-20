d229629f20a64c552859b132c1af71cdc72865b9
boonsboos <boons@boonsboos.nl>
2025-12-20 19:32:55 +0100

    [FSA-123] move delete user route to /users/me

--- src/main/kotlin/nl/connectplay/scoreplay/routes/users/Delete.kt
+++ src/main/kotlin/nl/connectplay/scoreplay/routes/users/Delete.kt
@@ -1,14 +1,10 @@
 package nl.connectplay.scoreplay.routes.users
 
-import io.ktor.http.*
 import io.ktor.server.auth.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import nl.connectplay.scoreplay.UserIdJWTAuthenticatorName
 import nl.connectplay.scoreplay.controllers.UserController
-import nl.connectplay.scoreplay.exceptions.UnauthorizedException
 import nl.connectplay.scoreplay.routes.ApiRoute
-import nl.connectplay.scoreplay.utilities.getUserIdFromJWT
 import org.koin.ktor.ext.inject
 
 @ApiRoute
@@ -16,15 +12,7 @@ fun Route.deleteUserRoute() {
     val userController: UserController by inject() // with the injection can we get a instance of the userController by Koin dependency
 
     authenticate(UserIdJWTAuthenticatorName) {
-        delete("/users/{id}") {
-            try {
-                if (call.getUserIdFromJWT() != call.parameters["id"]?.toInt())
-                    return@delete call.respond(HttpStatusCode.Forbidden, "You are not allowed to delete this user")
-            } catch (e: UnauthorizedException) {
-                call.application.environment.log.error("Authorization error while deleting user: ${e.message}")
-                call.respond(HttpStatusCode.Unauthorized)
-            }
-
+        delete("/users/me") {
             userController.handleDeleteUserAsync(call)
         }
     }
